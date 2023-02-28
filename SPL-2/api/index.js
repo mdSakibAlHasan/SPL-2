88f256c43014dcc7460578924c1bae1f552fb498
mdSakibAlHasan <bsse1209@iit.du.ac.bd>
2023-02-28 22:41:31 +0600

    node module delete here

--- SPL-2/api/index.js
+++ SPL-2/api/index.js
@@ -8,31 +8,15 @@ import cors from 'cors';
 
 
 import postROUTE from "./Path/post.js"
-//const cors = require('cors');
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
+app.use(cors());
 
 // //const upload = multer({ storage });
 
-
-
-// app.use(authRoutes);
-// app.use(userRoutes);
-
-
-// app.listen(3000, () => {
-//   console.log("Connected!");
-// });
-
-
-app.use(cors());
-
-// app.use(express.json());
-//app.use(express.text());
-//app.use(express.static(`${__dirname}/public/`));
 
 app.get('/',(req,res)=>{
     res.send('This is home page');
@@ -40,25 +24,7 @@ app.get('/',(req,res)=>{
 app.use('/p',postROUTE);
 app.use('/api',authRoutes);
 
-// app.post('/',(req,res)=>{
-//     //console.log(req.body);
-//     res.send('This is home page with post request');
-// });
 
-// const cors = require('cors');
-// const corsOptions ={
-//     origin:'http://localhost:3000', 
-//     credentials:true,            //access-control-allow-credentials:true
-//     optionSuccessStatus:200
-// }
-// app.use(cors(corsOptions));
-
-// const corsOptions ={
-//     origin:'http://localhost:3000', 
-//     credentials:true,            //access-control-allow-credentials:true
-//     optionSuccessStatus:200
-// }
-// app.use(cors(corsOptions));
 
 app.listen(3001,()=>{
     console.log('lisening on port 3000');
