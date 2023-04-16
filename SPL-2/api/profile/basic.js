14ab9c20294ab15d91c11cd8eb948860a46813b9
mdSakibAlHasan <bsse1209@iit.du.ac.bd>
2023-04-16 15:50:00 +0600

    institution researcher added

--- SPL-2/api/profile/basic.js
+++ SPL-2/api/profile/basic.js
@@ -7,14 +7,14 @@ export const getResearcher = (req,res) =>{
     const dept = req.body.dept;
     console.log("In get researcher",dept, req.body);
     const q = `SELECT DISTINCT(personal_info.ID), personal_info.name, designation, photo FROM sakib.personal_info, sakib.login, sakib.department where personal_info.ID = login.ID and  login.DID = (SELECT department.ID FROM sakib.department where name='${dept}');`;       //insert table name
-    db.query(q,function(err,result){
+    const qur = `select ID, Name, Photo,Designation from  bcsir.researcher, bcsir.department  where bcsir.researcher.departmentID = bcsir.department.DepartmentID and bcsir.department.DepartmentName ='${dept}' `;
+    console.log(qur);
+    db.query(qur,function(err,result){
       if(err){
         console.log("Something happend for check  personal info");
         return res.status(409).json("department not found ");
       }
       else{
-        // const namesArray = Object.keys(result).map(key => result[key].name);
-        // console.log(namesArray)
         console.log(result);
         return res.status(200).send(result);
       }
