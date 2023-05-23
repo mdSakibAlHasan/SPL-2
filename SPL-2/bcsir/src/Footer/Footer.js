a38420c98fe59fa4f0c3cd5189bbd5dfc1dcfcc6
fahimmahmud618 <75488772+fahimmahmud618@users.noreply.github.com>
2023-05-23 17:02:03 +0600

    CSS Update Footer.js

--- SPL-2/bcsir/src/Footer/Footer.js
+++ SPL-2/bcsir/src/Footer/Footer.js
@@ -3,16 +3,16 @@ import './Footer.css';
 import Logo from './or-logo.png'
 function Footer() {
   return (
-    <footer className="bg-dark text-light mt-auto py-3">
+    <footer className="shade3 p-5">
       <div className="container">
         <div className="row">
           <div className="col-md-4">
-            <h5>About Us</h5>
+            <center><h5 className='display-6'>About Us</h5><hr/></center>
             <img src={Logo} style={{height:"50px", width:"50px"}} alt="Organization Logo" />
             <p>Main objective of BCSIR is to pursue scientific research for the betterment of the Bangladeshi people. It was established on 16 November 1973.</p>
           </div>
           <div className="col-md-4">
-            <h5>Contact Us</h5>
+            <center><h5 className='display-6'>Contact Us</h5><hr/></center>
             <div className="d-flex justify-content-around">
               <a href="https://www.facebook.com/" className='fa fa-facebook' alt="Facebook" ></a>
               <a href="https://twitter.com/" className='fa fa-twitter' alt="Twitter"></a>
@@ -20,14 +20,15 @@ function Footer() {
             </div>
           </div>
           <div className="col-md-4">
-            <h5>Find Us</h5>
+            <center><h5 className='display-6'>Find Us</h5><hr/></center>
             <i className='fas fa-map-marker-alt'></i>
             <p>Dr. Qudrat-E-Khuda Road<br />Dhaka 1205</p>
           </div>
         </div>
       </div>
+      <hr/>
       <div className="container text-center">
-        <span className="text-muted">&copy; 2023 My Organization</span>
+        <big>&copy; 2023 || Bangladesh Council of Scientific and Industrial Research</big>
       </div>
     </footer>
   );
