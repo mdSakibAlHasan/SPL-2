d70835543efe95259cd795104303a8c1fc994a90
fahimmahmud618 <75488772+fahimmahmud618@users.noreply.github.com>
2023-05-26 17:38:03 +0600

    Update OtherJobExperience.js

--- SPL-2/bcsir/src/Set_up_profile/OtherJobExperience.js
+++ SPL-2/bcsir/src/Set_up_profile/OtherJobExperience.js
@@ -45,24 +45,23 @@ function OtherJobExperience() {
   };
 
   return (
-    <div className="container">
-      <h1>Other Job Experience</h1>
+    <div className='full_page_normal p-5 shade1'>
+    <div className="shade2 p-5 rounded">
+      <center><h4>Other Job Experience</h4></center> <hr /> <br/>
       {jobExperience.map((experience) => (
         <div key={experience.id}>
           <div className="form-group row">
-            <label className="col-sm-2 col-form-label">Serial No.</label>
-            <div className="col-sm-10">
-              <p>{experience.id}</p>
-            </div>
+            <h4 className="col-sm-">Serial No. {experience.id}</h4> <hr/>
+              
           </div>
           <div className="form-group row">
             <label className="col-sm-2 col-form-label">
               Short Job Description
             </label>
             <div className="col-sm-10">
-              <input
+              <textarea
                 type="text"
-                className="form-control"
+                
                 name="description"
                 value={experience.description}
                 onChange={(e) => handleJobExperienceChange(experience.id, e)}
@@ -74,7 +73,7 @@ function OtherJobExperience() {
             <div className="col-sm-10">
               <input
                 type="date"
-                className="form-control"
+                
                 name="fromDate"
                 value={experience.fromDate}
                 onChange={(e) => handleJobExperienceChange(experience.id, e)}
@@ -87,28 +86,20 @@ function OtherJobExperience() {
               <div className="input-group">
                 <input
                   type="date"
-                  className="form-control"
+                  
                   name="toDate"
                   value={experience.toDate}
                   onChange={(e) => handleJobExperienceChange(experience.id, e)}
                   disabled={experience.present}
                 />
-                <div className="input-group-append">
-                  <div className="input-group-text">
-                    <input
-                      type="checkbox"
-                      name="present"
-                      checked={experience.present}
-                      onChange={() => handlePresentChange(experience.id)}
-                    />{" "}
-                    Till now
-                  </div>
-                </div>
+               
+                 
+                  
               </div>
             </div>
           </div>
           <button
-            className="btn btn-danger"
+            className="m-2 btn btn-danger"
             onClick={() => removeJobExperience(experience.id)}
           >
             Remove
@@ -116,9 +107,19 @@ function OtherJobExperience() {
           <hr />
         </div>
       ))}
-      <button className="btn btn-primary" onClick={() => addJobExperience()}>
+      <button className="btn btn-outline-light" onClick={() => addJobExperience()}>
         Add Job Experience
       </button>
+      <br/><center>
+          {/* @sakib  onlcick add kore action/navigate korte hobe*/}
+          <button type="button" className="m-2 btn btn-outline-light">
+            Save Data
+          </button>
+          <button type="submit" className="m-2 btn btn-outline-light">
+            Go to Next Page
+          </button>
+        </center>
+    </div>
     </div>
   );
 }
